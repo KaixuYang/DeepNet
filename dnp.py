Simulation supports different variance, dnp bug fixes

dnp.py
@@ -155,7 +155,7 @@ class DeepNet:
                 loss.backward()
                 optimizer.step()
                 running_loss += loss.item()
-            else:
+            if e % 10 == 9:
                 print(f"Epoch: {e + 1}\nTraining loss: {running_loss/len(trainloader)}")
         self.last_model = copy.deepcopy(self.nnmodel)
 
@@ -276,7 +276,7 @@ class DeepNet:
             print(f"First iter, no Xavier initialization needed.")
 
     def train(self, x: torch.Tensor, y: torch.Tensor, batch_size: int = 100, learning_rate: float = 0.005,
-              epochs: int = 20, dropout_prop: list = None):
+              epochs: int = 20, dropout_prop: list = None, val: list = None):
         """
         train the deep neural network on x and y
         @param x: the training data
@@ -285,6 +285,7 @@ class DeepNet:
         @param learning_rate: learning rate
         @param epochs: number of epochs
         @param dropout_prop: a list of proportions of dropout in each hidden layer
+        @param val: a list of x_val and y_val
         """
         x = self.numpy_to_torch(x)
         y = self.numpy_to_torch(y)
@@ -300,6 +301,9 @@ class DeepNet:
             self.new = self.find_next_input(input_gradient)
             self.update_sets(self.new)
             print(f"Number of features selected is {len(self.S) - 1}.")
+            if val is not None:
+                pred = self.predict(val[0])
+                print(f"Validation accuracy is {np.mean(abs(pred.squeeze() - val[1].squeeze()))}")
         self.update_nn_weight(x, y)
         print(f"Feature selection completed, selected features are {self.selected}")
 

simulation.py
@@ -9,23 +9,26 @@ def sigmoid(x):
 	return np.exp(x) / (1 + np.exp(x))
 
 
-def generate_data(seed: int, m: int) -> (np.array, np.array, np.array, np.array):
+def generate_data(seed: int, m: int, n: int = 8500, train_size: int = 1000, flip: float = 0.05, sig: float = 0.5) \
+		-> (np.array, np.array, np.array, np.array):
 	"""
 	generate random simulation data according to the set up in "https://www.ijcai.org/proceedings/2017/0318.pdf"
+	@param train_size: training sample size
+	@param n: total sample size
 	@param m: nonzero input
 	@param seed: seed
+	@param flip: proportion of labels to be flipped
+	@param sig: the variance of nonzero weights
 	@return: x_train, x_test, y_train, y_test
 	"""
 	"""parameters"""
 	np.random.seed(seed)
 	p = 10000
-	n = 8500
-	train_size = 1000
 	h1 = 50
 	h2 = 30
 	h3 = 15
 	h4 = 10
-	sig = np.sqrt(0.5)
+	sig = np.sqrt(sig)
 	"""generate x"""
 	x = np.random.rand(n*p).reshape(n, p) * 2 - 1
 	"""neural network forward"""
@@ -45,8 +48,8 @@ def generate_data(seed: int, m: int) -> (np.array, np.array, np.array, np.array)
 	prob = sigmoid(np.matmul(a4, w5))
 	"""generate y"""
 	y = np.where(prob > 0.5, 1, 0)
-	"""flip 5% labels"""
-	flip = np.random.choice(range(n), size=int(n*0.05))
+	"""flip labels"""
+	flip = np.random.choice(range(n), size=int(n*flip))
 	y[flip, :] = 1 - y[flip, :]
 	print(f"y has mean {y.mean()}")
 	return x[:train_size, :], x[train_size:, :], y[:train_size], y[train_size:]